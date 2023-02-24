b63b4301501302ffb43bef81dbdf3645a942e18a
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-02-24 15:23:09 -0500

    add strikethrough styling

--- src/views/GroceryListPage.vue
+++ src/views/GroceryListPage.vue
@@ -7,16 +7,25 @@
       <div v-for="(item, index) in store.groceryList" :key="index" class="item">
         <div
           class="my-2 py-1 px-4 rounded flex flex-row justify-between"
-          :class="item.category.color"
+          :class="[
+            item.category.color,
+            item.completed ? 'strike-through' : false,
+          ]"
         >
-          <div class="cursor-pointer">
-            <font-awesome-icon icon="fa-regular fa-square" />
+          <div @click="store.toggleCompleted(index)" class="cursor-pointer">
+            <font-awesome-icon
+              :icon="
+                item.completed
+                  ? 'fa-regular fa-square-check'
+                  : 'fa-regular fa-square'
+              "
+            />
           </div>
           <div class="">
             <font-awesome-icon :icon="item.category.icon" />
           </div>
           <div class="">{{ item.item }}</div>
-          <div class="">Qty: {{ item.quantity }}</div>
+          <div class="">x {{ item.quantity }}</div>
           <div>{{ item.units }}</div>
           <div class="cursor-pointer">
             <font-awesome-icon icon="fa-solid fa-xmark" />
@@ -35,5 +44,17 @@ const store = useGroceryListStore();
 <style scoped>
 .item {
   width: 345px;
+}
+
+.strike-through {
+  position: relative;
+}
+.strike-through::before {
+  content: "";
+  border-bottom: 2px solid black;
+  width: 70%;
+  position: absolute;
+  right: 15%;
+  top: 50%;
 }
 </style>
