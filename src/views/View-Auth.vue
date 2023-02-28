c8d19da877ee8e1e93eca23c27110efb5a789004
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-02-28 10:45:51 -0500

    test

--- src/views/View-Auth.vue
+++ src/views/View-Auth.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mx-1 auth">
     <div
-      class="mb-8 text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700"
+      class="mb-8 text-sm font-medium text-center text-gray-500 border-b border-gray-200"
     >
       <ul class="flex justify-center -mb-px">
         <li class="mr-2">
@@ -10,8 +10,8 @@
             href="#"
             :class="
               !register
-                ? 'inline-block p-4 text-blue-500 border-b-2 border-blue-500 rounded-t-lg active dark:text-blue-500 dark:border-blue-500'
-                : 'inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
+                ? 'inline-block p-4 text-blue-500 border-b-2 border-blue-500 rounded-t-lg active'
+                : 'inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300'
             "
             >Login</a
           >
@@ -22,8 +22,8 @@
             href="#"
             :class="
               register
-                ? 'inline-block p-4 text-blue-500 border-b-2 border-blue-500 rounded-t-lg active dark:text-blue-500 dark:border-blue-500'
-                : 'inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
+                ? 'inline-block p-4 text-blue-500 border-b-2 border-blue-500 rounded-t-lg active'
+                : 'inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300'
             "
             >Register</a
           >
@@ -31,18 +31,16 @@
       </ul>
     </div>
     <div
-      class="mx-auto max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700"
+      class="mx-auto max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8"
     >
       <form @submit.prevent="onSubmit" class="space-y-6">
-        <h5
-          class="text-xl text-center font-medium text-gray-900 dark:text-white"
-        >
+        <h5 class="text-xl text-center font-medium text-gray-900">
           {{ formTitle }}
         </h5>
         <div>
           <label
             for="email"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            class="block mb-2 text-sm font-medium text-gray-900"
             >Email</label
           >
           <input
@@ -50,14 +48,14 @@
             type="email"
             name="email"
             id="email"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="name@company.com"
           />
         </div>
         <div>
           <label
             for="password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            class="block mb-2 text-sm font-medium text-gray-900"
             >Password</label
           >
           <input
@@ -66,13 +64,13 @@
             name="password"
             id="password"
             placeholder="••••••••"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           />
         </div>
         <div v-if="register">
           <label
             for="confirm"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            class="block mb-2 text-sm font-medium text-gray-900"
             >Confirm password</label
           >
           <input
@@ -81,13 +79,13 @@
             name="confirm"
             id="confirm"
             placeholder="••••••••"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           />
         </div>
 
         <button
           type="submit"
-          class="w-full text-white bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="w-full text-white bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         >
           {{ formTitle }}
         </button>
