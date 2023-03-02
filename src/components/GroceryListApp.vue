6dfc10b8141631d950d47d8fa82b1ba0dfe9c123
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-03-02 08:20:25 -0500

    add transition

--- src/components/GroceryListApp.vue
+++ src/components/GroceryListApp.vue
@@ -1,27 +1,29 @@
 <template>
   <div>
-    <div v-if="storeAuth.user.id" class="absolute bg-slate-200 h-12 w-full">
-      <div class="mx-auto max-w-screen-md h-full">
-        <p class="float-left text-xs mt-4 ml-4 md:ml-12">
-          Hello,
-          <strong
-            >{{
-              storeAuth.user.email.length > 26
-                ? `${storeAuth.user.email.slice(0, 26)}...`
-                : storeAuth.user.email
-            }}
-            👋</strong
+    <Transition name="slide-in">
+      <div v-if="storeAuth.user.id" class="absolute bg-slate-200 h-12 w-full">
+        <div class="mx-auto max-w-screen-md h-full">
+          <p class="float-left text-xs mt-4 ml-4 md:ml-12">
+            Hello,
+            <strong
+              >{{
+                storeAuth.user.email.length > 26
+                  ? `${storeAuth.user.email.slice(0, 26)}...`
+                  : storeAuth.user.email
+              }}
+              👋</strong
+            >
+          </p>
+          <button
+            @click="storeAuth.logoutUser"
+            class="float-right mr-4 mt-2 text-xs cursor-pointer shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-1.5 px-3 rounded md:mr-12"
+            :to="{ name: 'view-about' }"
           >
-        </p>
-        <button
-          @click="storeAuth.logoutUser"
-          class="float-right mr-4 mt-2 text-xs cursor-pointer shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-1.5 px-3 rounded md:mr-12"
-          :to="{ name: 'view-about' }"
-        >
-          Log out
-        </button>
+            Log out
+          </button>
+        </div>
       </div>
-    </div>
+    </Transition>
     <header class="flex items-center mb-6">
       <nav class="h-24 w-full flex justify-center pt-16 bg-gray-600 text-white">
         <RouterLink
@@ -53,4 +55,22 @@ const storeAuth = useStoreAuth();
 const infoTab = ref(false);
 </script>
 
-<style scoped></style>
+<style scoped>
+.slide-in-move, /* apply transition to moving elements */
+.slide-in-enter-active,
+.slide-in-active {
+  transition: all 0.5s ease;
+}
+
+.slide-in-enter-from,
+.slidein-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.slide-in-leave-active {
+  position: absolute;
+}
+</style>
